e3186d3bbbc4a60c51e2779c9df485f63cf1d626
rkhrn <rkhrn@gmail.com>
2020-10-17 00:09:19 +0530

    One Program Created

---
+++ Java Programs/Session 4/Test.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String args[]){
+            int x=7;
+            if(x==2);
+            System.out.println("NumberSeven");
+            System.out.println("NotSeven");
+    }
+}        
