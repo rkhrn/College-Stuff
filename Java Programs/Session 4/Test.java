56f7444fcfc028729d9f9c8a579f62dd7010ddd0
rkhrn <rkhrn@gmail.com>
2020-10-17 00:20:34 +0530

    delte

--- Java Programs/Session 4/Test.java
+++
@@ -1,8 +0,0 @@
-class Test{
-    public static void main(String args[]){
-            int x=7;
-            if(x==2);
-            System.out.println("NumberSeven");
-            System.out.println("NotSeven");
-    }
-}        
